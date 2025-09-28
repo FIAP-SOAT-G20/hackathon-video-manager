f7de00efc0f8b987e98e4440d06749842feeafc5
Alice Tomaz <tomaz.alice@gmail.com>
2025-09-27 22:34:28 -0300

    chore: update environment variables and dependencies

--- api-server.js
+++ api-server.js
@@ -9,12 +9,12 @@ const jwksRsa = require('jwks-rsa');
 
 const app = express();
 const port = process.env.API_PORT || 3001;
-const baseUrl = process.env.APP_BASE_URL;
+// const baseUrl = process.env.APP_BASE_URL;
 const domain = process.env.AUTH0_DOMAIN;
 const issuerBaseUrl = `https://${process.env.AUTH0_DOMAIN}`;
 const audience = process.env.AUTH0_AUDIENCE;
 
-if (!baseUrl || !domain) {
+if (!domain) {
   throw new Error('Please make sure that the file .env.local is in place and populated');
 }
 
